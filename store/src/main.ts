dff0e2781f7e3a137eeec29604a97fe4158b0578
Basil Kotov <basilkot@gmail.com>
2019-11-25 16:39:39 +0200

    move store preview script to module

--- store/src/main.ts
+++ store/src/main.ts
@@ -1,14 +1,14 @@
 import { Environment } from './environment';
 import { ServiceLocator } from './service-locator';
 
-document.addEventListener('DOMContentLoaded', () => {
-    console.log('run preview window');
-    console.log(document.cookie);
+function startApp() {
     Environment.DesignerUrl = window.location.search.substr(1).split('&').find(q => q.startsWith('ep=')).substr(3);
     const app = ServiceLocator.createApp();
     app.run();
-});
+}
 
 window.addEventListener('click', (event) => {
     event.preventDefault();
 });
+
+startApp();
